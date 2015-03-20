f1e8cf2c9f230f2971814a1ba183b19e6310a1bc
Chuck Obaze <chuckobaze@gmail.com>
2015-03-20 01:01:50 -0400

    No message

--- js/jquery_test_script.js
+++ js/jquery_test_script.js
@@ -7,6 +7,28 @@
 
 */
 
+/*
+	This example is for adding class
+
 $(document).ready(function(){
-	$('#celebs tbody tr:even').addClass('.even_colors');
+	$('#celebs tbody tr:even').css({
+		'background-color':'blue',
+		'color':'white'
+		});
+});
+
+$(document).ready(function(){
+	$('#celebs tbody tr:even').addClass('even_colors');
+});
+
+$(document).ready(function(){
+	$('#celebs tbody tr:even').removeClass('even_colors');
+});
+
+*/
+
+$(document).ready(function(){
+	$('#hideButton').click(function(){
+		$('p#disclaimer').hide();
+	});
 });
