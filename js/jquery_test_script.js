159d9365794ef6c78764b7efa8d2c0574d9a96f0
Chuck Obaze <chuckobaze@gmail.com>
2015-03-20 00:43:10 -0400

    No message

--- js/jquery_test_script.js
+++ js/jquery_test_script.js
@@ -1,7 +1,15 @@
-//$(document).ready(function(){
-  alert($('#celebs tr').length + ' elements!');
-});//
+/*
+	This example is for selecting
+	
+	$(document).ready(function(){
+	alert($('#celebs tbody tr').length + ' elements!');
+});
+
+*/
 
 $(document).ready(function(){
-	alert($('tr').length);
+	$('#celebs tbody tr:even').css({
+		'background-color':'blue',
+		'color':'white'
+		});
 });
